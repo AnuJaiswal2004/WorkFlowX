e63c90b616cc9a70ad4dde029a211387341ff226
Anubhav <anubhavjaiswal37@gmail.com>
2026-06-12 15:16:07 +0530

    Tasklist added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Login from './components/Auth/login.jsx'
+import TaskList from './components/TaskList/TaskList.jsx'
 
 const App = () => {
   return (
     <>
-    {<login />}
+      {/* <Login /> */}
+      <TaskList />
     </>
   )
 }
